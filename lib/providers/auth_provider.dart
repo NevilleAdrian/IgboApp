dcc1319f477e63ef085c0c0d2994b57773544096
NevilleAdrian <neville.chukumah@gmail.com>
2020-08-24 14:10:20 +0100

    ..

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:nkuzi_igbo/models/app_model.dart';
+import 'package:nkuzi_igbo/models/progres_model.dart';
 import 'package:nkuzi_igbo/models/user_model.dart';
 import 'package:nkuzi_igbo/repository/hive_repository.dart';
 import 'package:nkuzi_igbo/services/network_helper.dart';
@@ -12,20 +13,26 @@ class Auth extends ChangeNotifier {
   HiveRepository _hiveRepository = HiveRepository();
   User _user;
   String _token;
+  List<dynamic> _categories;
+
 
   User get user => _user;
   String get token => _token;
+  dynamic get category => _categories;
+
 
   setUser(User user) => _user = user;
   setToken(String token) => _token = token;
+  setCategory(List<dynamic> category) => _categories = category;
 
-  static Auth authProvider(BuildContext context, {bool listen = false}) =>
-      Provider.of<Auth>(context, listen: listen);
+  static Auth authProvider(BuildContext context, {bool listen = false}) => Provider.of<Auth>(context, listen: listen);
 
   Future<void> loginUser(String email, String password) async {
     try {
       var data = await _helper.loginUser(email, password);
+      var category = await _helper.getCategory();
       _setInitialData(data);
+      _categories = category['data'];
     } catch (ex) {
       throw Exception(ex);
     }
@@ -52,7 +59,6 @@ class Auth extends ChangeNotifier {
     setUser(User.fromJson(data['user']));
     setToken(data['token']);
     _hiveRepository.add<User>(name: kUserName, key: 'user', item: user);
-    _hiveRepository.add<AppModel>(
-        name: kAppDataName, key: 'appModel', item: AppModel(token: token));
+    _hiveRepository.add<AppModel>(name: kAppDataName, key: 'appModel', item: AppModel(token: token));
   }
 }
